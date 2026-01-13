f52f82bfa10915f38b12718365868752795f5ba7
Koji Kagawa <kagawa.koji@kagawa-u.ac.jp>
2026-01-13 18:18:05 +0900

    最初のコミット

---
+++ README.md
@@ -0,0 +1,21 @@
+﻿# ExtractComments
+
+HTML の中のコメントを抽出するシンプルなツールです。
+
+[Beatifulsoup4](https://www.crummy.com/software/BeautifulSoup/) を使用して HTML をパースし、コメントを抽出します。
+
+## インストール
+
+```bash 
+> python -m venv .venv
+> .venv\Scripts\activate  # Windows の場合
+> python -m pip install -r requirements.txt
+```
+
+## 使い方
+
+```bash
+> python extract_comments.py input.html
+or
+> python extract_comments.py https://example.com/input.html 
+```
---
+++ extract_comments.py
@@ -0,0 +1,30 @@
+﻿from bs4 import BeautifulSoup, Comment
+import requests
+import sys
+
+def extract_comments(content):
+    soup = BeautifulSoup(content, 'html.parser')
+    comments = soup.find_all(string=lambda text: isinstance(text, Comment))
+    return [comment.strip() for comment in comments]
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python extractComments.py <URL>")
+        sys.exit(1)
+    url = sys.argv[1]
+    if url.startswith('http://') or url.startswith('https://'):
+        response = requests.get(url)
+        if response.status_code == 200:
+            content = response.content
+            comments = extract_comments(content)
+            for comment in comments:
+                print(comment)
+        else:
+            print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+    else:
+        # Assume it's a local file path
+        with open(url, 'r', encoding='utf-8') as file:
+            content = file.read()
+            comments = extract_comments(content)
+            for comment in comments:
+                print(comment)
